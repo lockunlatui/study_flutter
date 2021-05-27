4d1c312fc8970f8e1f1efdc26eaa3ad201df8a79
Loc Do <lockunlatui@gmail.com>
2021-05-27 23:58:38 +0700

    add code

---
+++ expense_app/lib/widgets/chart.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import './chart_bar.dart';
+
+import '../models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      var totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        var recentTransactionDate = recentTransactions[i].date;
+
+        if (recentTransactionDate.day == weekDay.day &&
+            recentTransactionDate.month == weekDay.month &&
+            recentTransactionDate.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(
+        0.0, (sum, element) => sum += element['amount']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 5,
+        margin: EdgeInsets.all(20),
+        child: Padding(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransactionValues.map((groupTx) {
+              return Flexible(
+                  fit: FlexFit.tight,
+                  child: ChartBar(
+                      groupTx['day'],
+                      groupTx['amount'],
+                      totalSpending == 0.0
+                          ? 0.0
+                          : (groupTx['amount'] as double) / totalSpending));
+            }).toList(),
+          ),
+          padding: EdgeInsets.all(10),
+        ));
+  }
+}
--- expense_app/lib/widgets/new_transaction.dart
+++ expense_app/lib/widgets/new_transaction.dart
@@ -1,29 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    widget.addTx(enteredTitle, enteredAmount);
+
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Container(
+      child: SingleChildScrollView(
         child: Column(
           children: <Widget>[
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               // onChanged: (val) => titleInput = val,
               controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               // onChanged: (val) => amountInput = val,
               controller: amountController,
+              keyboardType: Platform.isAndroid
+                  ? TextInputType.number
+                  : TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => submitData(),
             ),
             TextButton(
-                onPressed: () {
-                  print('amountInut: ${amountController.text}');
-                },
+                onPressed: submitData,
                 child: Text(
                   'Add Transaction',
                   style: TextStyle(color: Colors.purple),
